79ac19f03a1c46d7755ac12495c2880399639ba5
Matt Kurtz <101678272+matt-kurtz@users.noreply.github.com>
2024-05-14 15:01:00 -0700

    Finished marking path for BFS search

--- A1/a1.py
+++ A1/a1.py
@@ -38,11 +38,7 @@ def bfs(arr, start_x, start_y, length_rows, length_cols):
     q.enqueue((start_x, start_y, 0))
     visited = set()
     visited.add((start_x, start_y))
-    #count = 0
-    path_length = 0
-    path_cost = 0
-    states_explored = 0
-    states_on_open_list = 0
+    parent = {}
     while not q.is_empty():
         #input("Press Enter to continue to the next iteration...")
         #os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console
@@ -50,9 +46,16 @@ def bfs(arr, start_x, start_y, length_rows, length_cols):
         #count += 1
         #printArray(arr)  # Print the maze
         current_x, current_y, path_length = q.dequeue()
-        path_cost += 1
         if arr[current_x][current_y] == 'E':
-            return current_x, current_y, arr, path_length, path_cost, states_explored, states_on_open_list
+            path_x = current_x
+            path_y = current_y
+            start_x, start_y = returnStart(arr)
+            while (path_x != start_x) or (path_y != start_y):
+                #print("We're in this while loop")
+                arr[path_x][path_y] = '*'
+                path_x, path_y = parent[(path_x, path_y)]
+            arr[current_x][current_y] = 'E'
+            return current_x, current_y, arr, path_length
         else:
             for dx, dy, direction in [(1, 0, 'v'), (-1, 0, '^'), (0, 1, '>'), (0, -1, '<')]:
                 new_x, new_y = current_x + dx, current_y + dy
@@ -61,32 +64,23 @@ def bfs(arr, start_x, start_y, length_rows, length_cols):
                         arr[new_x][new_y] = direction
                     q.enqueue((new_x, new_y, path_length + 1))
                     visited.add((new_x, new_y))
-                    states_on_open_list += 1
-        states_explored += 1
-        states_on_open_list += 1
-    return None, None, arr, path_length, path_cost, states_explored, states_on_open_list
+                    parent[(new_x, new_y)] = (current_x, current_y)
+    return None, None, arr, path_length
 
 
-# Testing input code
+# Testing input code 
 #filename = "pathFindingMap.txt"
 #arr = inputFile(filename)
 #printArray(arr)
 
 if __name__ == "__main__":
-    filename = input("Enter name of file: ")
-    if os.path.exists(filename):
-        arr = inputFile(filename)
-        printArray(arr)
-        length_rows, length_cols = length(arr)
-        start_x, start_y = returnStart(arr)
-        print("S is at", start_x, ",", start_y)
-        x, y, arr, path_length, path_cost, states_explored, states_on_open_list = bfs(arr, start_x, start_y, length_rows, length_cols)
-        print("End coordinates are:", x, ",", y)
-        print("Path length:", path_length)
-        print("Path Cost:", path_cost)
-        print("Number of states explored:", states_explored)
-        print("Number of states still on the open list:", states_on_open_list)
-        print("Finished maze is: ")
-        printArray(arr)
-    else:
-        print("File does not exist!")
+    #filename = input("Enter name of file: ")
+    filename = "pathFindingMap.txt"
+    arr = inputFile(filename)
+    printArray(arr)
+    length_rows, length_cols = length(arr)
+    start_x, start_y = returnStart(arr)
+    print("S is at", start_x, ",", start_y)
+    x, y, arr, path_length = bfs(arr, start_x, start_y, length_rows, length_cols)
+    print("End coordinates are:", x, ",", y)
+    printArray(arr)
